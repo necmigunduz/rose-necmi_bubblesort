6060e9028107695d82963392b4b3bcafe7cb8e3f
Necmi Gunduz <necmigunduz>
2021-01-06 13:50:33 +0300

    Added bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,3 +14,24 @@ def bubble_sort(arr)
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(arr)
+  len = arr.length - 1
+  while len
+    unsorted = false
+    (0...len).each do |i|
+      if yield(arr[i], arr[i + 1]).to_i >= 0
+        arr[i + 1], arr[i] = arr[i], arr[i + 1]
+        unsorted = true
+      end
+    end
+    break if !unsorted
+  end
+  arr
+end
+
+bubble = bubble_sort_by (%w[hello hey hi]) do |left, right|
+  left.length - right.length
+end
+
+puts bubble
